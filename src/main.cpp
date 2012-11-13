653cdb258ebe60ace17df845cb390f06c0ee5516
Jeff Lunt <jefflunt@gmail.com>
2012-11-13 11:03:27 -0600

    Major refactoring and code cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -16,165 +16,64 @@
 
 #include <sstream>
 
-//these can be set by command-line arguments
-bool PARSEGAMELISTONLY = false;
-bool IGNOREGAMELIST = false;
 bool DRAWFRAMERATE = false;
+bool keepRunning = true;
+unsigned int width = 0;
+unsigned int height = 0;
 
 namespace fs = boost::filesystem;
 
-int main(int argc, char* argv[])
-{
-	unsigned int width = 0;
-	unsigned int height = 0;
-	if(argc > 1)
-	{
-		for(int i = 1; i < argc; i++)
-		{
-			if(strcmp(argv[i], "-w") == 0)
-			{
-				width = atoi(argv[i + 1]);
-				i++;
-			}else if(strcmp(argv[i], "-h") == 0)
-			{
-				height = atoi(argv[i + 1]);
-				i++;
-			}else if(strcmp(argv[i], "--gamelist-only") == 0)
-			{
-				PARSEGAMELISTONLY = true;
-			}else if(strcmp(argv[i], "--ignore-gamelist") == 0)
-			{
-				IGNOREGAMELIST = true;
-			}else if(strcmp(argv[i], "--draw-framerate") == 0)
-			{
-				DRAWFRAMERATE = true;
-			}else if(strcmp(argv[i], "--help") == 0)
-			{
-				std::cout << "EmulationStation, a graphical front-end for ROM browsing.\n";
-				std::cout << "Command line arguments:\n";
-				std::cout << "-w [width in pixels]		set screen width\n";
-				std::cout << "-h [height in pixels]		set screen height\n";
-				std::cout << "--gamelist-only			skip automatic game detection, only read from gamelist.xml\n";
-				std::cout << "--ignore-gamelist		ignore the gamelist (useful for troubleshooting)\n";
-				std::cout << "--draw-framerate		display the framerate\n";
-				std::cout << "--help				summon a sentient, angry tuba\n\n";
-				std::cout << "More information available in README.md.\n";
-				return 0;
-			}
-		}
-	}
+int main(int argc, char* argv[]) {
+  init();
+  while (keepRunning) { runFrontEnd() }
+  shutdown();
+  
+	return 0;
+}
 
-
-	#ifdef _RPI_
-		bcm_host_init();
-	#endif
-
-	bool running = true;
-
-	//the renderer also takes care of setting up SDL for input and sound
-	bool renderInit = Renderer::init(width, height);
-	if(!renderInit)
-	{
-		std::cerr << "Error initializing renderer!\n";
-		return 1;
-	}
-
-
-	//initialize audio
+void init() {
+  processCmdLineArgs(argc, argv);
+  initSound();
+  initRendererOrDie(width, height);
 	AudioManager::init();
+  enableJoystick();
+  ensureConfigDirectoryExists();
+  processConfigOrDie();
+}
 
+void enableJoystick() {
+  SDL_JoystickEventState(SDL_ENABLE);
+}
 
-	SDL_JoystickEventState(SDL_ENABLE);
+void processConfig() {
+  if (configSettingsDoNotExist())	{
+	  createDemoConfig()
+	  keepRunning = false;
 
-	//make sure the config directory exists
-	std::string home = getenv("HOME");
-	std::string configDir = home + "/.emulationstation";
-	if(!fs::exists(configDir))
-	{
-		std::cout << "Creating config directory \"" << configDir << "\"\n";
-		fs::create_directory(configDir);
-	}
-
-
-	//try loading the system config file
-	if(!fs::exists(SystemData::getConfigPath())) //if it doesn't exist, create the example and quit
-	{
-		std::cerr << "A system config file in " << SystemData::getConfigPath() << " was not found. An example will be created.\n";
-		SystemData::writeExampleConfig();
-		std::cerr << "Set it up, then re-run EmulationStation.\n";
-		running = false;
-	}else{
-		SystemData::loadConfig();
-
-		if(SystemData::sSystemVector.size() == 0) //if it exists but was empty, notify the user and quit
+	} else if (configExistsButIsEmpty()) {
+		std::cerr << "A system config file in " << SystemData::getConfigPath() << " was found, but contained no systems.\n";
+		std::cerr << "Does at least one system have a game presesnt?\n";
+		keepRunning = false;
+	} else {
+	  //choose which GUI to open depending on Input configuration
+		if(fs::exists(InputManager::getConfigPath()))
 		{
-			std::cerr << "A system config file in " << SystemData::getConfigPath() << " was found, but contained no systems.\n";
-			std::cerr << "Does at least one system have a game presesnt?\n";
-			running = false;
+			//an input config already exists - load it and proceed to the gamelist as usual.
+			InputManager::loadConfig();
+			GuiGameList::create();
 		}else{
-			//choose which GUI to open depending on Input configuration
-			if(fs::exists(InputManager::getConfigPath()))
-			{
-				//an input config already exists - load it and proceed to the gamelist as usual.
-				InputManager::loadConfig();
+			//if no input.cfg is present, but a joystick is connected, launch the input config GUI
+			if(SDL_NumJoysticks() > 0)
+				new GuiInputConfig();
+			else
 				GuiGameList::create();
-			}else{
-				//if no input.cfg is present, but a joystick is connected, launch the input config GUI
-				if(SDL_NumJoysticks() > 0)
-					new GuiInputConfig();
-				else
-					GuiGameList::create();
 
-			}
 		}
 	}
+}
 
-	int lastTime = 0;
-	while(running)
-	{
-		SDL_Event event;
-		while(SDL_PollEvent(&event))
-		{
-			switch(event.type)
-			{
-				case SDL_JOYHATMOTION:
-				case SDL_JOYAXISMOTION:
-				case SDL_JOYBUTTONDOWN:
-				case SDL_JOYBUTTONUP:
-				case SDL_KEYDOWN:
-				case SDL_KEYUP:
-					InputManager::processEvent(&event);
-					break;
-
-				case SDL_QUIT:
-					running = false;
-					break;
-			}
-		}
-
-		int curTime = SDL_GetTicks();
-		int deltaTime = curTime - lastTime;
-		lastTime = curTime;
-
-		GuiComponent::processTicks(deltaTime);
-		Renderer::render();
-
-		if(DRAWFRAMERATE)
-		{
-			float framerate = 1/((float)deltaTime)*1000;
-			std::stringstream ss;
-			ss << framerate;
-			std::string fps;
-			ss >> fps;
-			Renderer::drawText(fps, 50, 50, 0x00FF00FF, Renderer::getDefaultFont(Renderer::MEDIUM));
-		}
-
-
-		Renderer::swapBuffers();
-	}
-
-
-	AudioManager::deinit();
+void shutdown() {
+  AudioManager::deinit();
 	Renderer::deleteAll();
 	Renderer::deinit();
 	SystemData::deleteSystems();
@@ -182,10 +81,115 @@ int main(int argc, char* argv[])
 	std::cout << "EmulationStation cleanly shutting down...\n";
 
 	SDL_Quit();
+  ShutdownSound();
+}
 
-	#ifdef _RPI_
+void runFrontEnd() {
+  int lastTime = 0;
+  SDL_Event event;
+	while(SDL_PollEvent(&event)) {
+		switch(event.type) {
+			case SDL_JOYHATMOTION:
+			case SDL_JOYAXISMOTION:
+			case SDL_JOYBUTTONDOWN:
+			case SDL_JOYBUTTONUP:
+			case SDL_KEYDOWN:
+			case SDL_KEYUP:
+				InputManager::processEvent(&event);
+				break;
+
+			case SDL_QUIT:
+				keepRunning = false;
+				break;
+		}
+	}
+
+	int curTime = SDL_GetTicks();
+	int deltaTime = curTime - lastTime;
+	lastTime = curTime;
+
+	GuiComponent::processTicks(deltaTime);
+	Renderer::render();
+
+	if (DRAWFRAMERATE) {
+		float framerate = 1/((float)deltaTime)*1000;
+		std::stringstream ss;
+		ss << framerate;
+		std::string fps;
+		ss >> fps;
+		Renderer::drawText(fps, 50, 50, 0x00FF00FF, Renderer::getDefaultFont(Renderer::MEDIUM));
+	}
+
+	Renderer::swapBuffers();
+}
+
+void processCmdLineArgs(argc, argv[]) {
+  if(argc > 1) {
+		for(int i = 1; i < argc; i++) {
+			if(strcmp(argv[i], "-w") == 0) {
+				width = atoi(argv[i + 1]);
+				i++;
+			} else if(strcmp(argv[i], "-h") == 0) {
+				height = atoi(argv[i + 1]);
+				i++;
+			} else if(strcmp(argv[i], "--draw-framerate") == 0) {
+				DRAWFRAMERATE = true;
+			} else if(strcmp(argv[i], "--help") == 0) {
+				std::cout << "EmulationStation, a graphical front-end for ROM browsing.\n";
+				std::cout << "Command line arguments:\n";
+				std::cout << "-w [width in pixels]		set screen width\n";
+				std::cout << "-h [height in pixels]		set screen height\n";
+				std::cout << "--draw-framerate		display the framerate\n";
+				std::cout << "--help				summon a sentient, angry tuba\n\n";
+				std::cout << "More information available in README.md.\n";
+				return 0;
+			}
+		}
+	}
+}
+
+// 
+void initSound() {
+  #ifdef _RPI_
+		bcm_host_init();
+	#endif
+}
+
+void shutdownSound() {
+  #ifdef _RPI_
 		bcm_host_deinit();
 	#endif
+}
 
-	return 0;
+void initRendererOrDie(width, height) {
+  bool renderInitSuccessful = Renderer::init(width, height);
+	if(!renderInitSuccessful) {
+		std::cerr << "Error initializing renderer!\n";
+		exit(1);
+	}
+}
+
+void ensureConfigDirectoryExists() {
+  std::string home = getenv("HOME");
+	std::string configDir = home + "/.emulationstation";
+	if(!fs::exists(configDir)) {
+		std::cout << "Creating config directory \"" << configDir << "\"\n";
+		fs::create_directory(configDir);
+	}
+}
+
+void createDemoConfig() {
+  void configSettingsDoNotExist 
+  std::cerr << "A system config file in " << SystemData::getConfigPath() << " was not found. An example will be created.\n";
+  SystemData::writeExampleConfig();
+  std::cerr << "Set it up, then re-run EmulationStation.\n";
+}
+
+bool configSettingsDoNotExist() {
+  return !fs::exists(SystemData::getConfigPath());
+}
+
+bool configExistsButIsEmpty() {
+  SystemData::loadConfig();
+  return SystemData::sSystemVector.size() == 0
 }
