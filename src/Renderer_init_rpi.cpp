22c9bcac9d59957f309d1aeaf729362d84bc281c
Jeff Lunt <jefflunt@gmail.com>
2012-12-15 11:16:19 -0600

    Added call to `flush`

--- src/Renderer_init_rpi.cpp
+++ src/Renderer_init_rpi.cpp
@@ -161,6 +161,7 @@ namespace Renderer
 		eglSwapBuffers(display, surface);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     std::cout << "RPI swapBuffers";
+    std::cout.flush();
 	}
 
 	void destroySurface()
